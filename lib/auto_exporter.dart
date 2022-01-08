7fb8391c48d0da5a6a76b683a52ce9d4d48e2d9b
劉達 <ryu@anchorz.co.jp>
2022-01-08 14:37:53 +0900

    update file name

---
+++ lib/auto_exporter.dart
@@ -0,0 +1,21 @@
+library flutter_auto_export;
+
+import 'package:build/build.dart';
+
+import 'src/ExporterGeneratorBuilder.dart';
+import 'src/ExportsBuilder.dart';
+
+export 'package:auto_exporter/src/AutoExporter.dart';
+
+// ignore: public_member_api_docs
+const version = '1.0.2';
+
+// ignore: public_member_api_docs
+Builder exporterGeneratorBuilder(BuilderOptions options) {
+  return ExporterGeneratorBuilder();
+}
+
+// ignore: public_member_api_docs
+Builder exportsBuilder(BuilderOptions options) {
+  return ExportsBuilder();
+}

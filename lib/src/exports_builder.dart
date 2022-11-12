f78dd71f05eff7770a7ff9fb54a6aa2361844edc
normidar <36730656+normidar@users.noreply.github.com>
2022-11-12 22:48:31 +0900

    Update exports_builder.dart

--- lib/src/exports_builder.dart
+++ lib/src/exports_builder.dart
@@ -18,7 +18,8 @@ class ExportsBuilder implements Builder {
 
     final expList = <String>[];
     final content = [
-      "// run this to reset your file: dart run build_runner build",
+      "// run this to reset your file:  dart run build_runner build",
+      "// or use flutter:               flutter packages pub run build_runner build",
       "// remenber to format this file, you can use: dart format",
       "// publish your package hint: dart pub publish --dry-run",
       "// if you want to update your packages on power: dart pub upgrade --major-versions",
